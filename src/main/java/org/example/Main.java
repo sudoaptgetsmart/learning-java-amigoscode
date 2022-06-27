fa1c02d365d98ae0cb54099b31e715f01078049b
sudoaptgetsmart <tfrye463@gmail.com>
2022-06-27 17:34:49 -0500

    added string types

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.Locale;
 
@@ -24,7 +25,7 @@ public class Main {
         System.out.println("isAdult = " + isAdult);
         System.out.println("nameInitial = " + nameInitial);
 
-        // Non primitive data types AKA reference types
+        // Non-primitive data types AKA reference types
         String name = new String("Amigoscode");
         char c = 'A';
         System.out.println(name.toUpperCase());
@@ -59,16 +60,57 @@ public class Main {
         System.out.println("after changing mariam: ");
         System.out.println("alex = " + alex.name);
         System.out.println("mariam = " + mariam.name);
-    }
-        // reference types
-        static class Person {
-            String name;
 
-            Person(String name) {
-                this.name = name;
-            }
+        // Naming variables (should make sense)
+        int zero = 0;
+        int one = 1;
+        double pi = 3.1415;
+        // an underscore (_) makes it easier to read but will print normal in console
+        int subscriberCount = 1_000_000;
+
+        // The String Class
+        // String is the class
+            // myName is the object
+        String myName = new String("Taylor");
+        // with Strings, you don't need "new String()"
+            // below will do the same thing
+        String myNameWithoutNew = ("Samuel");
+        System.out.println("myName = " + myName);
+        System.out.println("myName contains lor = " + myName.contains("lor"));
+        String sam = "Sam";
+        System.out.println("myNameWithoutNew = " + myNameWithoutNew);
+        // eslint will alert that below will always equal true and suggest simplifying
+        System.out.println("myNameWithoutNew contains sam = " + myNameWithoutNew.contains(sam));
+        System.out.println("myNameWithoutNew equals sam = " + myNameWithoutNew.equals(sam));
+        // simplified version below
+        System.out.println("myNameWithoutNew equals sam simplified = " + false);
+
+        //Concatenating Strings
+        String firstName = "Alex";
+        String lastName = "Jamir";
+        String fullName = firstName + " " + lastName;
+        System.out.println("fullName = " + fullName);
+        // this does the same thing
+        String fullName2 = firstName.concat(" ").concat(lastName);
+        System.out.println("fillName2 = " + fullName2);
+
+        // Packages & Import Statement
+        Date date = new Date();
+        LocalDate localDate = LocalDate.now();
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println("date = " + date);
+        System.out.println("localDate = " + localDate);
+        System.out.println("localDateTime = " + localDateTime);
+
+
+    }
+
+    // reference types
+    static class Person {
+        String name;
+
+        Person(String name) {
+            this.name = name;
         }
-
-
-
     }
+}
